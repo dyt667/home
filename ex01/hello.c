c0672d1e8985f164b6665e8741d42af10e463eaa
dyt667 <18227377157@163.com>
2026-04-30 22:38:28 +0800

    commit 2: add input function to read and print user input

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char input_content[100];  
+    printf("请输入内容：");
+    scanf("%s", input_content); 
+    printf("你输入的内容是：%s\n", input_content);
     return 0;
 }
